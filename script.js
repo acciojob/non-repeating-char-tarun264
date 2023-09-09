16412858e323913194e149ec6603156020875e8e
Tarun Rana <66361151+tarun264@users.noreply.github.com>
2023-09-10 00:10:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+ let freq = new Array(256).fill(0);
+    for(let i = 0; i < str.length; i++){
+        freq[str.charCodeAt(i)]++;
+    }
+    let ans = null;
+    for(let i = 0; i < str.length; i++){
+        if(freq[str.charCodeAt(i)] == 1){
+            ans = str.charAt(i);
+            break;
+        }
+    }
+return ans;
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+  const input = prompt("Enter a string");
+  alert(firstNonRepeatedChar(input)); 
